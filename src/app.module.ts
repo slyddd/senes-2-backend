db630424f38440e7bbffe0554a004ae8733e28ab
slyddd <dayvidnavarrete@gmail.com>
2025-02-25 18:45:52 -0500

    feat: ✔️ Add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ParticipantModule } from './participant/participant.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
-  imports: [],
+  imports: [ParticipantModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.NODE_ENV === 'development' ? 'mysql' : 'mssql',
+      url: process.env.DB_URL,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
